e18d4506cfeb96fa675777042d019f1ceed94541
Radoslaw Jurczak <radoslawjurczak95@gmail.com>
2023-11-02 18:43:53 +0100

    Add data generation utils and the main script
    Signed-off-by: Radoslaw Jurczak <radoslawjurczak95@gmail.com>

--- code/utils.py
+++ code/utils.py
@@ -9,7 +9,8 @@ import ujson
 def generate_random_record() -> str:
     return ujson.dumps(
         {
-            "id": str(uuid.uuid4()),
+            "user_id": str(uuid.uuid4()),
+            "comment_id": random.randint(0, 1_000_000),
             "name": names.get_full_name(),
             "age": random.randint(18, 100),
             "content": generate_random_text(100),
